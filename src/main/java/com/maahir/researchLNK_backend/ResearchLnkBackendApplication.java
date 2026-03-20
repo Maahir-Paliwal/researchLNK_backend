75148122db0dd4a60167188292b9d6b5fbe54ce2
Maahir-Paliwal <Maahir.paliwal@mail.mcgill.ca>
2026-03-20 00:29:56 -0400

    first commit

---
+++ src/main/java/com/maahir/researchLNK_backend/ResearchLnkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.maahir.researchLNK_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchLnkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchLnkBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maahir/researchLNK_backend/ResearchLnkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maahir.researchLNK_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchLnkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
