44d4ab23fd767d14ed7d410f2570207818999691
Maahir-Paliwal <Maahir.paliwal@mail.mcgill.ca>
2026-03-21 01:54:53 -0400

    TOFO: Create specific entity/join table for conversation including metadata similar to connection table

--- src/main/java/com/maahir/researchLNK_backend/persistence/model/Conversation.java
+++ src/main/java/com/maahir/researchLNK_backend/persistence/model/Conversation.java
@@ -1,4 +1,30 @@
 package com.maahir.researchLNK_backend.persistence.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+import java.util.Set;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "conversations")
 public class Conversation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name="created_at")
+    @CreationTimestamp
+    private Instant createdAt;
 }
