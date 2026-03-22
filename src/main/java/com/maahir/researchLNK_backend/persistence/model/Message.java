25c34715b5a80cda8e300c0c762e4ff6caf907ba
Maahir-Paliwal <Maahir.paliwal@mail.mcgill.ca>
2026-03-21 21:59:34 -0400

    messaging service completed

--- src/main/java/com/maahir/researchLNK_backend/persistence/model/Message.java
+++ src/main/java/com/maahir/researchLNK_backend/persistence/model/Message.java
@@ -1,4 +1,37 @@
 package com.maahir.researchLNK_backend.persistence.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "messages")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="conversation_id", nullable = false)
+    private Conversation conversation;
+
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable = false)
+    private User user;
+
+    @Column(name = "content", nullable = false, updatable = false)
+    private String content;
+
+    @Column(name="created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private Instant createdAt;
+
 }
