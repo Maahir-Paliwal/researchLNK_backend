5e0bbdff23ff9abc5bb8736027a56d202cc298e5
Maahir-Paliwal <Maahir.paliwal@mail.mcgill.ca>
2026-03-22 22:34:15 -0400

    shell created for repository test for connection. Necessary dependencies installed. Repo tests will be run with @DataJpaTest, and with @TestContainers to not interfere with actual DB.

--- src/test/java/com/maahir/researchLNK_backend/repository/ConnectionRepositoryTest.java
+++ src/test/java/com/maahir/researchLNK_backend/repository/ConnectionRepositoryTest.java
@@ -1,7 +1,20 @@
 package com.maahir.researchLNK_backend.repository;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+
+@DataJpaTest
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class ConnectionRepositoryTest {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("pgvector/pgvector:pg17");
 }
