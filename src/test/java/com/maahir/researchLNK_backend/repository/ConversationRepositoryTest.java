e7262db5b1c3e9123e7ebe3ec0eb61558e35e535
Maahir-Paliwal <Maahir.paliwal@mail.mcgill.ca>
2026-04-04 21:21:32 -0400

    updated convo test

--- src/test/java/com/maahir/researchLNK_backend/repository/ConversationRepositoryTest.java
+++ src/test/java/com/maahir/researchLNK_backend/repository/ConversationRepositoryTest.java
@@ -1,8 +1,12 @@
 package com.maahir.researchLNK_backend.repository;
 
 import com.maahir.researchLNK_backend.persistence.model.Conversation;
+import com.maahir.researchLNK_backend.persistence.model.User;
+import com.maahir.researchLNK_backend.persistence.model.enums.Role;
 import com.maahir.researchLNK_backend.persistence.repository.ConversationRepository;
+import com.maahir.researchLNK_backend.persistence.repository.UserRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
@@ -29,14 +33,36 @@ public class ConversationRepositoryTest {
     @Autowired
     private ConversationRepository conversationRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    private User creator;
+
     @AfterEach
     public void clearDatabase() {
         conversationRepository.deleteAll();
+        userRepository.deleteAll();
     }
+
+    @BeforeEach
+    public void setUp() {
+        creator = User.builder()
+                .userName("creator")
+                .email("creator@test.com")
+                .passwordHash("pw1")
+                .role(Role.RESEARCHER)
+                .isActive(true)
+                .name("creator")
+                .build();
+
+        creator = userRepository.save(creator);
+    }
+
 
     @Test
     public void testPersistConversation() {
         Conversation conversation = Conversation.builder()
+                .creator(creator)
                 .title("title")
                 .build();
         conversation = conversationRepository.save(conversation);
@@ -48,6 +74,7 @@ public class ConversationRepositoryTest {
         assertThat(conversationOptional).isPresent();
         assertThat(conversationOptional.get().getId()).isEqualTo(conversation.getId());
         assertThat(conversationOptional.get().getTitle()).isEqualTo(conversation.getTitle());
+        assertThat(conversationOptional.get().getCreator().getId()).isEqualTo(conversation.getCreator().getId());
         assertThat(conversationOptional.get().getCreatedAt()).isNotNull();
     }
 }
